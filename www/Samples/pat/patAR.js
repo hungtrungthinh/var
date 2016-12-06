7a120c0d1f6b4aaaf6754e1e6f80c9a187489f8c
Patrick Hiller <patrick.hiller@blippar.com>
2016-12-06 11:03:01 -0500

    testing

--- www/Samples/pat/patAR.js
+++ www/Samples/pat/patAR.js
@@ -3,3 +3,10 @@ var v_ar = new VisageAR();
 var container = document.getElementById('main');
 var webcam = document.getElementById('webcam');
 
+var Module = 
+{
+	onRuntimeInitialized: function()
+	{
+		onModuleInitialized();
+	}
+};
